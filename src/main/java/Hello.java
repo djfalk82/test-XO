ba5b7f201054f8694a8e473adc6eae87a0472bcf
dfalk <dfalk@myune.edu.au>
2022-08-12 21:31:25 +1000

    changing print output.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] arg)
     {
 
-        System.out.println("my name is dog");
+        System.out.println("this is the author");
         new XO();
     }
 }
